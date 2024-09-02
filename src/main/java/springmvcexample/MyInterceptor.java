dc6850cd22be9beac6774f7ff8c26697bc570ec0
ankit-at-github <ankit.kumar.pune@gmail.com>
2024-09-02 13:10:01 -0400

    Spring Interceptor

---
+++ src/main/java/springmvcexample/MyInterceptor.java
@@ -0,0 +1,15 @@
+package springmvcexample;
+
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("This is preHandler..");
+        return true;
+    }
+}
